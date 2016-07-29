cccd6474256fa37017d85bb1a508be12580b99ae
omer-demir <farukomer.demir@hepsiburada.com>
2016-07-29 23:23:50 +0300

    changes

--- DMS.Api/Startup.cs
+++ DMS.Api/Startup.cs
@@ -9,11 +9,7 @@ namespace DMS.Api {
         public void Configuration(IAppBuilder appBuilder) {
             // Configure Web API for self-host. 
             HttpConfiguration config = new HttpConfiguration();
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+            config.MapHttpAttributeRoutes();
 
 
 
