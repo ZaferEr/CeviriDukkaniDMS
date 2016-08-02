d1d57d9f35396818f43efe21a0ce1114f173f4f9
omer-demir <farukomer.demir@hepsiburada.com>
2016-08-02 11:59:55 +0300

    changes

--- DMS.Business/Services/IDocumentService.cs
+++ DMS.Business/Services/IDocumentService.cs
@@ -5,17 +5,17 @@ using Tangent.CeviriDukkani.Domain.Dto.Response;
 
 namespace DMS.Business.Services {
     public interface IDocumentService {
-        ServiceResult AddTranslationDocument(TranslationDocumentDto documentDto, int createdBy);
+        ServiceResult<TranslationDocumentDto> AddTranslationDocument(TranslationDocumentDto documentDto, int createdBy);
         ServiceResult<TranslationDocumentDto> GetTranslationDocument(int documentId);
-        ServiceResult EditTranslationDocument(TranslationDocumentDto documentDto, int createdBy);
+        ServiceResult<TranslationDocumentDto> EditTranslationDocument(TranslationDocumentDto documentDto, int createdBy);
         ServiceResult<List<TranslationDocumentDto>> GetTranslationDocuments();
-        ServiceResult AddGeneralDocument(GeneralDocumentDto documentDto, int createdBy);
+        ServiceResult<GeneralDocumentDto> AddGeneralDocument(GeneralDocumentDto documentDto, int createdBy);
         ServiceResult<GeneralDocumentDto> GetGeneralDocument(int documentId);
-        ServiceResult EditGeneralDocument(GeneralDocumentDto documentDto, int createdBy);
+        ServiceResult<GeneralDocumentDto> EditGeneralDocument(GeneralDocumentDto documentDto, int createdBy);
         ServiceResult<List<GeneralDocumentDto>> GetGeneralDocuments();
-        ServiceResult AddUserDocument(UserDocumentDto documentDto, int createdBy);
+        ServiceResult<UserDocumentDto> AddUserDocument(UserDocumentDto documentDto, int createdBy);
         ServiceResult<UserDocumentDto> GetUserDocument(int documentId);
-        ServiceResult EditUserDocument(UserDocumentDto documentDto, int createdBy);
+        ServiceResult<UserDocumentDto> EditUserDocument(UserDocumentDto documentDto, int createdBy);
         ServiceResult<List<UserDocumentDto>> GetUserDocuments();
         ServiceResult<DocumentUploadResponseDto> AnalyzeDocument(string localFolder, string fileName);
         ServiceResult<List<TranslationDocumentPartDto>> GetDocumentPartsNormalized(int translationDocumentId, int partCount, int createdBy);
